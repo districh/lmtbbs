eb5001c5cdffe7f991ed9592ccd47a8e091faff4
Liyuan <w3390221@sina.com>
2018-03-23 03:43:33 +0000

    basic user profile editing

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'email' => 'required|email',
+            'introduction' => 'max:80',
+        ];
+    }
+}
