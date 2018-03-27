0b0d34d33f640db20682e0199d96ee64ae15bb78
Liyuan <w3390221@sina.com>
2018-03-27 03:09:19 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LMTBBS') - Laravel 进阶教程</title>
+    <meta name="description" content="@yield('description', 'LMTBBS 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
